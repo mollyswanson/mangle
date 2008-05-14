687898dbb424ca5b1dc22881fda86f50ee389288
Molly Swanson <mollyswanson@alum.mit.edu>
2008-05-14 22:26:51 +0000

    added environment script, fixed complement, trim, and matlab scripts --Molly
    git-svn-id: https://giordano.mit.edu/nvs/mangle/trunk/dev_80bit@78 9f78a35e-8a50-488b-8db1-b351ce30c53d

--- scripts/graphmask.sh
+++ scripts/graphmask.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-# Â© M E C Swanson 2007
+# © M E C Swanson 2008
 #plot the angular mask described by a polygon file in .list format
 #using the matlab mapping toolbox
 #optional 3rd argument turns on drawing black outlines around each polygon
+#if specifying the full path to a file (rather than just running in the directory 
+#containing the file you want to plot), put the path in double-quotes as shown below.
 #graphmask.sh <infile> <outfile> [<outlines>]
 #EXAMPLES: 
 #no outlines: graphmask.sh "dr4/safe0/sdss_dr4safe0_mask.list" "dr4/safe0/sdss_dr4safe0_mask.eps"
-#outlines: graphmask.sh "allsky_3s.list" "allsky_3s.eps" on
+#outlines: graphmask.sh allsky_3s.list allsky_3s.eps on
 
+if [ "$MANGLESCRIPTSDIR" = "" ] ; then
+    MANGLESCRIPTSDIR="../../scripts"
+fi
 
-matlab -nodisplay -r graphmask\(\'$1\',\'$2\',\'$3\'\)
+matlab -nodisplay -r addpath\(\'$MANGLESCRIPTSDIR\'\)\;graphmask\(\'$1\',\'$2\',\'$3\'\)
 echo all done!
