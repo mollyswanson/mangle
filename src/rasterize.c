221d35d4793fcf2efd3a7830f1fb0405f5c88127
Molly Swanson <mollyswanson@alum.mit.edu>
2010-04-16 14:38:42 +0000

    added free(weights) line in rasterize.c --Molly
    git-svn-id: https://giordano.mit.edu/nvs/mangle/trunk/dev_80bit@256 9f78a35e-8a50-488b-8db1-b351ce30c53d

--- src/rasterize.c
+++ src/rasterize.c
@@ -192,9 +192,10 @@ int main(int argc, char *argv[])
   }
   
   /* free array */
-   for(k = 0; k < npoly; k++){
-     free_poly(polys[k]);
+  for(k = 0; k < npoly; k++){
+    free_poly(polys[k]);
   }
+  free(weights);
   return(0);
 }
 
