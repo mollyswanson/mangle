0a0d4d155fd95bbe4f2e2e3fde8b3a449323f464
Molly Swanson <mollyswanson@alum.mit.edu>
2010-10-29 21:28:29 +0000

    rewriting trim_mask.sh to use rasterize -T and take snap and mtol arguments --Molly
    git-svn-id: https://giordano.mit.edu/nvs/mangle/trunk/dev_80bit@272 9f78a35e-8a50-488b-8db1-b351ce30c53d

--- scripts/trim_mask.sh
+++ scripts/trim_mask.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
 # © M E C Swanson 2008
 #script to trim a mask so that it only includes polygons in a specified region
-#USAGE: trim_mask.sh <mask> <trimmer> <outfile> [<pixelization arguments>]
+#USAGE: trim_mask.sh <mask> <trimmer> <outfile> [<pixelization arguments>] [<mtol arguments> <snap arguments>]
 #EXAMPLE:trim_mask.sh mask.pol trimmer.pol trimmed_mask.pol
 # You can also optionally provide arguments to the pixelize function
 # in the 4th argument:
 #EXAMPLE: trim_mask.sh mask.pol trimmer.pol trimmed_mask.pol -Ps0,6
-
 
 if [ "$MANGLEBINDIR" = "" ] ; then
     MANGLEBINDIR="../../bin"
@@ -22,15 +21,22 @@ mask=$1
 trimmer=$2
 outfile=$3
 pixargs=$4
+mtol=$5
+snap="$6 $7 $8"
 
 #check command line arguments:
 if [ "$mask" = "" ] || [ "$trimmer" = "" ] || [ "$outfile" = "" ] ; then
     echo >&2 "ERROR: enter the input polygon file, a polygon defining the region you" 
     echo >&2 "want to trim to, and the output polygon file as command line arguments."
     echo >&2 "" 
-    echo >&2 "USAGE: trim_mask.sh <mask> <trimmer> <outfile> [<pixelization arguments>]"
+    echo >&2 "USAGE: trim_mask.sh <mask> <trimmer> <outfile> [<pixelization arguments>] [<mtol arguments> <snap arguments>]"
     echo >&2 "EXAMPLE: trim_mask.sh mask.pol trimmer.pol trimmed_mask.pol"
     echo >&2 "EXAMPLE w/ optional argument to pixelize: trim_mask.sh mask.pol trimmer.pol trimmed_mask.pol -Ps0,6"   
+    echo >&2 "or to use non-default values for snap and multiple intersection tolerances:" 
+    echo >&2 "EXAMPLE: trim_mask.sh mask.pol trimmer.pol trimmed_mask.pol -Ps0,6 -m1e-8 -a.027 -b.027 -t.027"
+    echo >&2 "or to use non-default values for snap and multiple intersection tolerances w/o pixelize argument:" 
+    echo >&2 "EXAMPLE: trim_mask.sh mask.pol trimmer.pol trimmed_mask.pol \"\" -m1e-8 -a.027 -b.027 -t.027"
+
     exit 1
 fi
 
@@ -73,8 +79,8 @@ elif [ ! "$res1" = "" ] && [ "$res2" = "" ] ; then
 	echo >&2 "e.g. -Ps0,8, and try again."
 	exit 1
     fi
-    echo "$MANGLEBINDIR/pixelize -P${scheme1}0,$res1 $trimmer trimmer_pix"
-    $MANGLEBINDIR/pixelize -P${scheme1}0,$res1 $trimmer trimmer_pix || exit
+    echo "$MANGLEBINDIR/pixelize -P${scheme1}0,$res1 $mtol $trimmer trimmer_pix"
+    $MANGLEBINDIR/pixelize -P${scheme1}0,$res1 $mtol $trimmer trimmer_pix || exit
     trimmer="trimmer_pix"
 elif [ "$res1" = "" ] && [ ! "$res2" = "" ] ; then
     if [ $res2 -eq -1 ] ; then
@@ -83,55 +89,48 @@ elif [ "$res1" = "" ] && [ ! "$res2" = "" ] ; then
 	echo >&2 "e.g. -Ps0,8, and try again."
 	exit 1
     fi
-    echo "$MANGLEBINDIR/pixelize  -P${scheme2}0,$res2 $mask mask_pix"
-    $MANGLEBINDIR/pixelize  -P${scheme2}0,$res2 $mask mask_pix || exit
+    echo "$MANGLEBINDIR/pixelize -P${scheme2}0,$res2 $mtol $mask mask_pix"
+    $MANGLEBINDIR/pixelize -P${scheme2}0,$res2 $mtol $mask mask_pix || exit
     mask="mask_pix"
 else
-    echo "$MANGLEBINDIR/pixelize $pixargs $trimmer trimmer_pix"
-    $MANGLEBINDIR/pixelize $pixargs $trimmer trimmer_pix || exit
-    echo "$MANGLEBINDIR/pixelize $pixargs $mask mask_pix"
-    $MANGLEBINDIR/pixelize $pixargs $mask mask_pix || exit
+    echo "$MANGLEBINDIR/pixelize $pixargs $mtol $trimmer trimmer_pix"
+    $MANGLEBINDIR/pixelize $pixargs $mtol $trimmer trimmer_pix || exit
+    echo "$MANGLEBINDIR/pixelize $pixargs $mtol $mask mask_pix"
+    $MANGLEBINDIR/pixelize $pixargs $mtol $mask mask_pix || exit
     trimmer="trimmer_pix"
     mask="mask_pix"
 fi
 
-#check if input file is snapped
+#check if mask file is snapped
 snapped=`awk '/snapped/{print $1}' < $mask`
-
 #if mask file isn't snapped, snap it
 if [ ! "$snapped" = "snapped" ]; then
     echo "Snapping $mask ..."
     mv $mask jp
-    $MANGLEBINDIR/snap jp $mask  || exit
+    echo "$MANGLEBINDIR/snap $snap $mtol jp $mask"
+    $MANGLEBINDIR/snap $snap $mtol jp $mask  || exit
     rm jp
 fi
 
-#find the complement of the trimmer polygon
-echo "$MANGLESCRIPTSDIR/find_complement.sh $trimmer trimmer_comp"
-$MANGLESCRIPTSDIR/find_complement.sh $trimmer trimmer_comp || exit
+#check if trimmer file is snapped
+snapped=`awk '/snapped/{print $1}' < $trimmer`
+#if trimmer file isn't snapped, snap it
+if [ ! "$snapped" = "snapped" ]; then
+    echo "Snapping $trimmer ..."
+    mv $trimmer jp
+    echo "$MANGLEBINDIR/snap $snap $mtol jp $trimmer"
+    $MANGLEBINDIR/snap $snap $mtol jp $trimmer  || exit
+    rm jp
+fi
 
-#set the complement to have zero weight
-echo 0 > jw0
-echo "$MANGLEBINDIR/weight -zjw0 trimmer_comp jcomp0"
-$MANGLEBINDIR/weight -zjw0 trimmer_comp jcomp0 || exit
-rm jw0
-
-#balkanize the mask with the zero-weight complement, thereby trimming off 
-#everything that does not lie within the trimmer polygon 
-echo "$MANGLEBINDIR/balkanize $mask jcomp0 jb"
-$MANGLEBINDIR/balkanize $mask jcomp0 jb || exit
-rm jcomp0
-
-#unify to get rid of zero weight polygons
-echo "$MANGLEBINDIR/unify jb $outfile"
-$MANGLEBINDIR/unify jb $outfile || exit
-rm jb
+#trim using the rasterize "slice" option with -T
+echo "$MANGLEBINDIR/rasterize -T $mtol $trimmer $mask $outfile"
+$MANGLEBINDIR/rasterize -T $mtol $trimmer $mask $outfile || exit
 
 echo "Polygons of $1 trimmed by $2 written to ${outfile}."
 
 #clean up
 
-rm trimmer_comp
 if [ -e trimmer_pix ] ; then 
     rm trimmer_pix
 fi
